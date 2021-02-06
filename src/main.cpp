196db348d28b8db7bdb6a7d1f5173cbbdf797757
Griffin Sisk <gasisk@MacBook-Pro.local>
2021-02-05 21:32:40 -0500

    platformio setup

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <stdio.h>
+#define onboard 13
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  printf("CURocket\n");
+
+}
